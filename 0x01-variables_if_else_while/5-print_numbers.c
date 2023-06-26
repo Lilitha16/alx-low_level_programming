fdb5e25fa190508ed29bd1d89f66f894191fc777
Lilitha16 <litha.msutu@gmail.com>
2023-06-26 12:33:28 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c = 'a';
-
-    while (c <= 'z') {
-        if (c != 'q' && c != 'e')
-            putchar(c);
-        c++;
-    }
-
-    putchar('\n');
-    return 0;
+char c = 'a';
+while (c <= 'z'
+{
+if (c != 'q' && c != 'e')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
